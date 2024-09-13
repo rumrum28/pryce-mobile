e0621dab2e286ad41dadee48d9619b43f79efd7b
rumrum28 <crspyfry@gmail.com>
2024-09-13 11:35:54 +0800

    remove minor errors

--- app/(drawer)/shop/index.tsx
+++ app/(drawer)/shop/index.tsx
@@ -189,7 +189,7 @@ export default function Page() {
 
         {/* <ProductGroup /> */}
 
-        <Products />
+        <Products products={data?.productsResponse} />
 
         <Text
           style={{
